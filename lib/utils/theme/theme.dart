335ddc21e65b725832d665689e28a47cf667cc9d
AyaIbrahim3027 <ayaibrahim3027@gmail.com>
2024-09-13 17:54:28 +0200

    Add bottomSheetTheme

--- lib/utils/theme/theme.dart
+++ lib/utils/theme/theme.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce_app/utils/theme/custom_themes/appbar_theme.dart';
+import 'package:ecommerce_app/utils/theme/custom_themes/bottom_sheet_theme.dart';
 import 'package:ecommerce_app/utils/theme/custom_themes/elevated_button_theme.dart';
 import 'package:ecommerce_app/utils/theme/custom_themes/text_theme.dart';
 import 'package:flutter/material.dart';
@@ -15,6 +16,7 @@ class AppTheme {
     textTheme: AppTextTheme.lightTextTheme,
     elevatedButtonTheme:AppElevatedButtonTheme.lightElevatedButtonTheme,
     appBarTheme: AppbarTheme.lightAppBarTheme,
+    bottomSheetTheme: AppBottomSheetTheme.lightBottomSheetTheme,
   );
   static ThemeData darkTheme = ThemeData(
     useMaterial3: true,
@@ -25,5 +27,6 @@ class AppTheme {
     textTheme: AppTextTheme.darkTextTheme,
     elevatedButtonTheme:AppElevatedButtonTheme.darkElevatedButtonTheme,
     appBarTheme: AppbarTheme.darkAppBarTheme,
+    bottomSheetTheme: AppBottomSheetTheme.darkBottomSheetTheme,
   );
 }
