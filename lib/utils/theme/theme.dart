fbadf7d45596b153a4a8c44cfb9fdfa262900d6c
AyaIbrahim3027 <ayaibrahim3027@gmail.com>
2024-09-13 19:58:54 +0200

    Add outlinedButtonTheme

--- lib/utils/theme/theme.dart
+++ lib/utils/theme/theme.dart
@@ -3,6 +3,7 @@ import 'package:ecommerce_app/utils/theme/custom_themes/bottom_sheet_theme.dart'
 import 'package:ecommerce_app/utils/theme/custom_themes/checkbox_theme.dart';
 import 'package:ecommerce_app/utils/theme/custom_themes/chip_theme.dart';
 import 'package:ecommerce_app/utils/theme/custom_themes/elevated_button_theme.dart';
+import 'package:ecommerce_app/utils/theme/custom_themes/outlined_button_theme.dart';
 import 'package:ecommerce_app/utils/theme/custom_themes/text_field_theme.dart';
 import 'package:ecommerce_app/utils/theme/custom_themes/text_theme.dart';
 import 'package:flutter/material.dart';
@@ -23,6 +24,7 @@ class AppTheme {
     checkboxTheme: AppCheckboxTheme.lightCheckboxTheme,
     chipTheme: AppChipTheme.lightChipTheme,
     inputDecorationTheme: AppTextFormFieldTheme.lightInputDecorationTheme,
+    outlinedButtonTheme: AppOutlinedButtonTheme.lightOutlinedButtonTheme,
   );
   static ThemeData darkTheme = ThemeData(
     useMaterial3: true,
@@ -37,5 +39,6 @@ class AppTheme {
     checkboxTheme: AppCheckboxTheme.darkCheckboxTheme,
     chipTheme: AppChipTheme.darkChipTheme,
     inputDecorationTheme: AppTextFormFieldTheme.darkInputDecorationTheme,
+    outlinedButtonTheme: AppOutlinedButtonTheme.darkOutlinedButtonTheme,
   );
 }
