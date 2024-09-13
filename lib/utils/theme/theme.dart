0f516c9d22a42b23182673e5068a138508d41a92
AyaIbrahim3027 <ayaibrahim3027@gmail.com>
2024-09-13 19:50:02 +0200

    Add TextFormFieldTheme

--- lib/utils/theme/theme.dart
+++ lib/utils/theme/theme.dart
@@ -3,6 +3,7 @@ import 'package:ecommerce_app/utils/theme/custom_themes/bottom_sheet_theme.dart'
 import 'package:ecommerce_app/utils/theme/custom_themes/checkbox_theme.dart';
 import 'package:ecommerce_app/utils/theme/custom_themes/chip_theme.dart';
 import 'package:ecommerce_app/utils/theme/custom_themes/elevated_button_theme.dart';
+import 'package:ecommerce_app/utils/theme/custom_themes/text_field_theme.dart';
 import 'package:ecommerce_app/utils/theme/custom_themes/text_theme.dart';
 import 'package:flutter/material.dart';
 
@@ -21,6 +22,7 @@ class AppTheme {
     bottomSheetTheme: AppBottomSheetTheme.lightBottomSheetTheme,
     checkboxTheme: AppCheckboxTheme.lightCheckboxTheme,
     chipTheme: AppChipTheme.lightChipTheme,
+    inputDecorationTheme: AppTextFormFieldTheme.lightInputDecorationTheme,
   );
   static ThemeData darkTheme = ThemeData(
     useMaterial3: true,
@@ -34,5 +36,6 @@ class AppTheme {
     bottomSheetTheme: AppBottomSheetTheme.darkBottomSheetTheme,
     checkboxTheme: AppCheckboxTheme.darkCheckboxTheme,
     chipTheme: AppChipTheme.darkChipTheme,
+    inputDecorationTheme: AppTextFormFieldTheme.darkInputDecorationTheme,
   );
 }
