3d3c17e8d314e91315488725321b2656b120c983
AyaIbrahim3027 <ayaibrahim3027@gmail.com>
2024-09-22 16:34:32 +0200

    Update BrandNameWithVerifiedIcon

--- lib/common/widgets/brand_name_with_icon.dart
+++ lib/common/widgets/brand_name_with_icon.dart
@@ -1,33 +1,44 @@
+import 'package:ecommerce_app/common/widgets/brand_title_text.dart';
 import 'package:ecommerce_app/utils/constants/colors.dart';
+import 'package:ecommerce_app/utils/constants/enums.dart';
 import 'package:ecommerce_app/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
-class BrandNameWithIcon extends StatelessWidget {
-  const BrandNameWithIcon({
+class BrandNameWithVerifiedIcon extends StatelessWidget {
+  const BrandNameWithVerifiedIcon({
     super.key,
     required this.brandName,
-    required this.icon,
+    this.maxLines = 1,
+    this.textColor,
+    this.iconColor = ColorManager.primaryColor,
+    this.textAlign = TextAlign.center,
+    this.brandTextSize = TextSizes.small,
   });
 
   final String brandName;
-  final IconData icon;
+  final int maxLines;
+  final Color? textColor, iconColor;
+  final TextAlign? textAlign;
+  final TextSizes brandTextSize;
 
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Text(
-          brandName,
-          overflow: TextOverflow.ellipsis,
-          maxLines: 1,
-          style: Theme.of(context).textTheme.labelMedium,
+        BrandTitleText(
+          brandName: brandName,
+          color: textColor,
+          maxLines: maxLines,
+          textAlign: textAlign,
+          brandTextSize: brandTextSize,
         ),
         const SizedBox(
           width: AppSizes.xs,
         ),
         Icon(
-          icon,
-          color: ColorManager.primaryColor,
+          Iconsax.verify5,
+          color: iconColor,
           size: AppSizes.iconXs,
         ),
       ],
