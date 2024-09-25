690d3ed5c8f3239cb3ae8be6fb79dfe0f91c31cf
AyaIbrahim3027 <ayaibrahim3027@gmail.com>
2024-09-25 15:37:57 +0200

    Create Image Slider in Product Details Screen

--- lib/features/shop/screens/product_details/widgets/product_details_screen_body_widget.dart
+++ lib/features/shop/screens/product_details/widgets/product_details_screen_body_widget.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce_app/common/widgets/custom_shapes/curved_edges_widget.dart';
+import 'package:ecommerce_app/common/widgets/rounded_image_container.dart';
 import 'package:ecommerce_app/utils/constants/colors.dart';
 import 'package:ecommerce_app/utils/constants/image_strings.dart';
 import 'package:ecommerce_app/utils/constants/sizes.dart';
@@ -19,10 +20,10 @@ class ProductDetailsScreenBodyWidget extends StatelessWidget {
           CurvedEdgesWidget(
             widget: Container(
               color: dark ? ColorManager.darkerGrey : ColorManager.light,
-              child: const Stack(
+              child: Stack(
                 children: [
                   // Main image
-                  SizedBox(
+                  const SizedBox(
                     height: AppSizes.h_400,
                     child: Padding(
                       padding: EdgeInsets.all(AppSizes.productImageRadius * 2),
@@ -30,6 +31,32 @@ class ProductDetailsScreenBodyWidget extends StatelessWidget {
                         child: Image(
                           image: AssetImage(ImageManager.productImage1),
                         ),
+                      ),
+                    ),
+                  ),
+
+                  // Image Slider
+                  Positioned(
+                    right: 0,
+                    bottom: 30,
+                    child: SizedBox(
+                      height: AppSizes.h_80,
+                      child: ListView.separated(
+                        itemCount: 6,
+                        scrollDirection: Axis.horizontal,
+                        physics: const BouncingScrollPhysics(),
+                        itemBuilder: (_, int index) {
+                          return RoundedImageContainer(
+                              width: AppSizes.w_80,
+                              padding: const EdgeInsets.all(AppSizes.sm),
+                              border:
+                                  Border.all(color: ColorManager.primaryColor),
+                              backgroundColor:
+                                  dark ? ColorManager.dark : ColorManager.white,
+                              image: ImageManager.productImage1);
+                        },
+                        separatorBuilder: (_, __) =>
+                            const SizedBox(width: AppSizes.spaceBtwItems),
                       ),
                     ),
                   ),
