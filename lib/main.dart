25d220acb49e82850c2c50edc3807847609027b3
AyaIbrahim3027 <ayaibrahim3027@gmail.com>
2024-10-14 16:34:11 +0200

    Setup Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:ecommerce_app/app.dart';
+import 'package:ecommerce_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  // await Firebase.initializeApp(
+  //   options: DefaultFirebaseOptions.currentPlatform,
+  // ).then((FirebaseApp value)=> Get.put(AuthenticationRepository()));
+
   runApp(const App());
 }
-
-
